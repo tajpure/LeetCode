c6b99c34ee00725d5e0c17f0427e281d891c8b43
tajpure <tajpure@gmail.com>
2014-11-08 12:18:33 +0800

    add a problem

---
+++ src/tajpure/WordBreak.java
@@ -0,0 +1,17 @@
+package tajpure;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class WordBreak {
+
+    public boolean wordBreak(String s, Set<String> dict) {
+
+    	return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "leetcode";
+        Set<String> dict = new HashSet<String>();
+    }
+}
