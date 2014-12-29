3b02f90e8cc3a2d0934b4bbf36947aaf21e94f51
tajpure <tajpure@gmail.com>
2014-12-29 09:27:20 +0800

    Add Annotation

--- src/tajpure/MedianofTwoSortedArraysI.java
+++ src/tajpure/MedianofTwoSortedArraysI.java
@@ -1,5 +1,19 @@
 package tajpure;
 
+/**
+ * The basic idea is that if you are given two arrays A and B
+ * and know the length of each, you can check whether an element 
+ * A[i] is the median in constant time. Suppose that the median 
+ * is A[i]. Since the array is sorted, it is also greater than
+ * exactly j = [n/2] - (i - 1) elements in B. It requires constant
+ * time to check if B[j] <= A[i] <= B[j+1}. If A[i] is not the 
+ * median, then depending on whether A[i] is greater or less than 
+ * B[j] and B[j+1], you know that A[i] is either greater than or 
+ * less that the median.(This solution comes from MIT 6.046J / 
+ * 18.410J Introduction to Algorithms)
+ * @author taojx
+ *
+ */
 public class MedianofTwoSortedArraysI {
 	
 	public static double findMedianSortedArrays(int A[], int B[]) {
@@ -11,16 +25,11 @@ public class MedianofTwoSortedArraysI {
 	public static double medianSearch(int A[], int B[], int left, int right) {
 		int m = A.length;
 		int n = B.length;
-		System.out.println("l "+left+" r "+right);
 		if (left > right) return medianSearch(B, A, Math.max(1, (m+n)/2-m), Math.min(n, (m+n)/2));
 		int i = (left+right)/2;
 		int j = (m+n)/2-i;
-		System.out.println("i "+i+" j "+j);
-		System.out.println("A[i] "+A[i]+" B[j] "+B[j]);
 		if ((j == 0 || A[i] > B[j]) && (j == n || A[i] <= B[j+1])) {
-//			if (((m+n)&1) == 1) return A[i];
 			return A[i];
-//			else {return (A[i]+Math.min(A[i+1], B[j]))/2.0;}
 		} else if((j == 0 || A[i] > B[j]) && j != n && A[i] > B[j+1]) {
 			return medianSearch(A, B, left, i-1);
 		} else {
@@ -30,7 +39,7 @@ public class MedianofTwoSortedArraysI {
 	
 	public static void main(String[] args) {
 		int[] A = {2,4,6,32,38};
-		int[] B = {1,2,3,4};
+		int[] B = {1,2,3,4,5};
 		System.out.println(findMedianSortedArrays(A, B));
 	}
 }
