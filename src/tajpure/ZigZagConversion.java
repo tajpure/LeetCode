27345ec4ed3ada4ab048b21e971380e262232f41
tajpure <tajpure@gmail.com>
2014-12-29 19:20:22 +0800

    Add Annotation

--- src/tajpure/ZigZagConversion.java
+++ src/tajpure/ZigZagConversion.java
@@ -1,7 +1,16 @@
 package tajpure;
 
 /**
- *
+ * time : O(n), memory : O(1)
+ * @###############@############ <- The first row
+ * #@#############@#@##########
+ * ##@###########@###@#########  The upper part
+ * ###@#########@#####@########
+ * ####@#######@#######@####### <- The mid row
+ * #####@#####@#########@######
+ * ######@###@###########@#####  The lower part
+ * #######@#@#############@####
+ * ########@################@### <- The last row
  * @author tajpure
  *
  */
@@ -10,24 +19,33 @@ public class ZigZagConversion {
 	public static String convert(String s, int nRows) {
 		if (nRows == 1) return s;
 		int length = s.length();
-		String result = "";
-    	boolean top = false;
+		StringBuilder result = new StringBuilder("");
+		// To judge the length of the current step
+    	boolean isShortInterval;
+    	
         for (int i = 0; i < nRows; i++) {
         	int step = Math.max((nRows - i - 1) * 2, i * 2);
-        	top = i < nRows / 2 ? false : true;
+        	// The step of the upper part(1~nRows/2) should be started with long,
+        	// The step of the lower part(nRows/2+1~nRows) should be started with short. 
+        	isShortInterval = i < nRows / 2 ? false : true;
         	for (int j = i; j < length;) {
-            	result = result + s.charAt(j);
-            	if (top && i !=0 && i != nRows-1) {
+        		result.append(s.charAt(j));
+        		// All the step length of the first and last rows is same.
+            	if (isShortInterval && i !=0 && i != nRows-1) {	
+            		 // The short step.
             		 j = (nRows - 1) * 2 - step + j;
-            		 top = false;
+            		 isShortInterval = false; // The next step should be long
             	} else {
+            		// The long step
             		j = step + j;
-            		if (i !=0 && i != nRows-1)
-           		 		top = true;
+            		if (i !=0 && i != nRows-1) {
+            			isShortInterval = true; // The next step should be short
+            		}
             	}
         	}
         }
-		return result;
+        
+		return result.toString();
     }
 	
 	public static void main(String[] args) {
